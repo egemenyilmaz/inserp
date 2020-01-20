9ab422e28ae9d7976848d1a0f2bb30a60ef02525
ego <egemeny1053@gmail.com>
2020-01-20 22:06:42 +0300

    ravenize

---
+++ inserp/src/main/java/com/valurin/os/App.java
@@ -0,0 +1,12 @@
+package com.valurin.os;
+
+import javax.ws.rs.GET;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
